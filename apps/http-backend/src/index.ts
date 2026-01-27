705a62b82c94bb816dc3e6e7fc6b97411896920b
sbansal05 <siddhi.bansal0504@gmail.com>
2026-01-27 22:34:02 +0530

    Added common packages

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,11 +1,19 @@
 import express from "express";
 import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
+import {CreateUserSchema, SigninSchema} from "@repo/common/types"
 const app = express();
 
 app.post("/signup", (req, res) => {
-    //db call
+
+    const data = CreateUserSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
     res.json({
         userId: '123'
     })
@@ -13,6 +21,13 @@ app.post("/signup", (req, res) => {
 })
 
 app.post("/signin", (req, res) => {
+    const data = SigninSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
     const userId = 1;
     const token= jwt.sign({
         userId
@@ -25,7 +40,13 @@ app.post("/signin", (req, res) => {
 })
 
 app.post("/room", middleware, (req, res) => {
-    //db call
+    const data = CreateUserSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
     res.json({
         roomId: 123
     })
--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,14 +1,16 @@
 import { NextFunction, Request, Response } from "express";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 import jwt from "jsonwebtoken";
 
-interface JwtUserPayload {
-    userId: string;
-}
+
 export function middleware(req: Request, res: Response, next: NextFunction) {
     const token = req.headers["authorization"] ?? "";
 
-    const decoded  = jwt.verify(token, JWT_SECRET) as JwtUserPayload;
+    const decoded  = jwt.verify(token, JWT_SECRET) 
+
+    if (typeof decoded == "string") {
+        return;
+    }
 
     if (decoded) {
         req.userId = decoded.userId;
